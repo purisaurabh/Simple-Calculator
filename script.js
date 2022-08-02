21d808802c0d8150dd80c466c15cd0b1a5dcac34
purisaurabh <purisaurabh2885@gmail.com>
2022-08-03 01:12:37 +0530

    Commited

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var num1 = document.getElementById("first");
+var num2 = document.getElementById("second");
+
+
+var add_no = document.getElementById("add");
+var sub_no = document.getElementById("sub");
+var mul_no = document.getElementById("mul");
+var div_no = document.getElementById("div");
+
+var answer = document.getElementById("ans")
+
+add_no.addEventListener("click" , function()
+{
+    answer.value = parseInt(num1.value) + parseInt(num1.value);
+});
+
+sub.addEventListener('click' , function()
+{
+    answer.value  = parseInt(num1.value) - parseInt(num1.value);
+});
+
+mul.addEventListener('click' , function()
+{
+    answer.value  = parseInt(num1.value) * parseInt(num1.value);
+});
+
+div.addEventListener('click' , function()
+{
+    answer.value  = parseInt(num1.value) / parseInt(num1.value);
+});
+
