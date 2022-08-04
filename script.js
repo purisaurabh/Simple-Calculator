92309f9185de8acd084d5aecb9671b50e1b7f9ff
purisaurabh <purisaurabh2885@gmail.com>
2022-08-04 18:19:33 +0530

    Commit

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ var answer = document.getElementById("ans")
 
 add_no.addEventListener("click" , function()
 {
-    answer.value = parseInt(num1.value) + parseInt(num1.value);
+    answer.value = parseInt(num1.value) + parseInt(num2.value);
 });
 
 sub.addEventListener('click' , function()
 {
-    answer.value  = parseInt(num1.value) - parseInt(num1.value);
+    answer.value  = parseInt(num1.value) - parseInt(num2.value);
 });
 
 mul.addEventListener('click' , function()
 {
-    answer.value  = parseInt(num1.value) * parseInt(num1.value);
+    answer.value  = parseInt(num1.value) * parseInt(num2.value);
 });
 
 div.addEventListener('click' , function()
 {
-    answer.value  = parseInt(num1.value) / parseInt(num1.value);
+    answer.value  = parseInt(num1.value) / parseInt(num2.value);
 });
 
